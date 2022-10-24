fe9687bbf84ec7e44c5ce05c73e78152c278c960
Aaron-Cunningham <aaroncunningham222@gmail.com>
2022-10-24 18:44:59 +0100

    Added menu options

---
+++ menu.py
@@ -0,0 +1,16 @@
+class menu:
+    def menu():
+        print("************************************************")
+        print()
+        print("             Bank Management Console           ")
+        print()
+        print("************************************************\n"
+              )
+        print("Select 1 to view all clients\n"
+              "Select 2 to remove account holders\n"
+              "Select 3 to add a new account\n"
+              "Select 4 to add money to an account\n"
+              "Select 5 to remove money to an account\n"
+              "Select 6 to edit a clients deatils\n"
+              "\n"
+              "************************************************")
