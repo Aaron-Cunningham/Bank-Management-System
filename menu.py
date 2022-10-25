18f893f77996ccfeb267b16f67c8362e40f56789
Aaron-Cunningham <aaroncunningham222@gmail.com>
2022-10-25 01:58:45 +0100

    Made indexing false

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import pandas as pd
+from pathlib import Path
 from datetime import datetime
 import random
 from self import self
@@ -35,7 +35,7 @@ class menu:
             option = int(input("Enter your choice: "))
         except:
             print("Enter int values only")
-            menu.menu()
+            menu.menu(self)
 
         # Menu option buttons
         if option == 1:
@@ -47,14 +47,14 @@ class menu:
         elif option == 2:
             menu.remove(self)
         elif option == 3:
-            menu.add(self)
+            menu.addClient(self)
         elif option != default:
             print("Only input a number specified")
             menu.menu(self)
 
     def viewClients(self):
         """Views clients in the CSV file"""
-        clients = pd.read_csv("../data/client_data.csv")
+        clients = pd.read_csv("data/client_data.csv")
         print(clients)
         print("Would you like to return to the menu?\n"
               "1: Yes\n"
@@ -66,13 +66,13 @@ class menu:
             exit()
 
     def remove(self):
-        clients = pd.read_csv("../data/client_data.csv")
+        clients = pd.read_csv("data/client_data.csv")
 
         print(clients)
 
         AccountNumber = int(input("Enter the account number of the account you wish to delete"))
         clients.drop(clients[clients['Account Number'] == AccountNumber].index, inplace=True)
-        # clients.to_csv("data/client_data.csv")
+        clients.to_csv("data/client_data.csv", index = False)
         print(clients)
         print("****************************************\n"
               "           Client deleted                \n"
@@ -86,19 +86,19 @@ class menu:
         elif option == 2:
             exit()
 
-    def add(self):
+    def addClient(self):
         """Adds a new client to the CSV file/Bank"""
         # User inputs when adding a new client
         firstName = input("Enter First Name: ")
         lastName = input("Enter Last Name: ")
         title = input("Enter Title: ")
         occupation = input("Enter Occupation: ")
-        accountNumber = random.randint(4000,4999)
+        accountNumber = random.randint(4000, 4999)
         # While loop until user enters right format
         while True:
             # Try except when user enters wrong values
             try:
-                birthday = input("Enter Birthday in this format dd/m/yyyy: ")
+                birthday = "29/01/1999" #input("Enter Birthday in this format dd/m/yyyy: ")
                 dateOfBirth = datetime.strptime(birthday, '%d/%m/%Y').date()
                 # Breaks out of loop
                 break
@@ -121,10 +121,11 @@ class menu:
                 print("Enter only int values")
 
         # Reads CSV file
-        clients = pd.read_csv("../data/client_data.csv")
+        clients = pd.read_csv("data/client_data.csv")
         # Adds data based on what was inputted
-        clients.loc[len(clients)]=[firstName, lastName, title, accountNumber, dateOfBirth, occupation, accountBalance, overDraftLimit]
-        clients.to_csv("data/client_data.csv")
+        clients.loc[len(clients)] = [firstName, lastName, title, accountNumber, dateOfBirth, occupation, accountBalance,
+                                     overDraftLimit]
+        clients.to_csv("data/client_data.csv", index = False)
         print(clients)
         print("****************************************\n"
               "        Successfully Added Client               \n"
