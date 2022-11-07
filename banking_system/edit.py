066bbc41b30479dfd874867c6a9ff039123b59fe
aaronc991 <93551130+aaronc991@users.noreply.github.com>
2022-11-07 23:05:55 +0000

    Added account checker

--- banking_system/edit.py
+++ banking_system/edit.py
@@ -5,74 +5,83 @@ import pandas as pd
 
 class Edit():
     """This class is used to edit clients details"""
+
     def __init__(self, account):
         # Instance variables set private
         self.__account = account
         self.__client = pd.read_csv("data/client_data.csv")
 
+    @property
+    def account_check(self):
+        """This checks that an account exists before proceeding with methods to change details"""
+        try:
+            self.get_account
+            if self.get_account.empty:
+                raise Exception
+        except:
+            print("*" * 35)
+            print("No account found, please try again")
+            print("*" * 35)
+            exit()
+
+    @property
     def get_account(self):
         """Gets the account of the clients account number"""
 
         return self.__client.loc[self.__client['Account Number'] == self.__account]
 
     def set_first_name(self, new_name):
+        self.account_check
         print("*" * 108)
         print("Old account details account details: \n"
-              "\n", self.get_account())
+              "\n", self.get_account)
         print()
         self.__client.loc[(self.__client['Account Number'] == self.__account), 'First name'] = new_name
         self.__client.to_csv("data/client_data.csv", index=False)
         print("New account details: \n"
-              "\n", self.get_account())
+              "\n", self.get_account)
         print("*" * 108)
 
     def set_last_name(self, new_name):
+        self.account_check
+
         print("*" * 108)
         print("Old account details account details: \n"
-              "\n", self.get_account())
+              "\n", self.get_account)
         print()
         self.__client.loc[(self.__client['Account Number'] == self.__account), 'Last name'] = new_name
         self.__client.to_csv("data/client_data.csv", index=False)
         print("New account details: \n"
-              "\n", self.get_account())
+              "\n", self.get_account)
         print("*" * 108)
 
     def set_occupation(self, new_occupation):
+        """Changes the occupation of an existing client"""
+        self.account_check
         print("*" * 108)
         print("Old account details account details: \n"
-              "\n", self.get_account())
+              "\n", self.get_account)
         print()
         self.__client.loc[(self.__client['Account Number'] == self.__account), 'Occupation'] = new_occupation
         self.__client.to_csv("data/client_data.csv", index=False)
         print("New account details: \n"
-              "\n", self.get_account())
+              "\n", self.get_account)
         print("*" * 108)
-
-    def add_account(self, first_name, last_name, title, account_number, date_of_birth, occupation, account_balance,
-                    overdraft_limit):
-        self.__client.loc[len(self.__client)] = [first_name, last_name, title, account_number, date_of_birth,
-                                                 occupation,
-                                                 account_balance,
-                                                 overdraft_limit]
-        __accountHolder = Edit(self.__client.loc[self.__client['Account Number'] == account_number])
-        print("*" * 108)
-        print("You just added the account:")
-        print()
-        print(__accountHolder.__account)
-        print("*" * 108)
-
-        self.__client.to_csv("data/client_data.csv", index=False)
 
     def negative(self):
-        self.__client < 0
+        """This method only shows clients with a negative balance"""
+        neg = self.__client < 0
+        # If there are no clients with negative balance this print will run
+        if neg.empty:
+            print("No clients with negative balance")
 
 
-
-class Add():
-
+class Add:
 
     def __init__(self, first_name, last_name, title, account_number, date_of_birth, occupation, account_balance,
                  overdraft_limit):
+
+        # Exceptions with type errors if wrong information is inputted on the object
         if not isinstance(first_name, str):
             raise TypeError("First name should be a string")
         if not isinstance(last_name, str):
@@ -89,7 +98,7 @@ class Add():
             raise TypeError("Account balance should be an Integer")
         if not isinstance(overdraft_limit, int):
             raise TypeError("Overdraft limit should be an Integer")
-
+        # Properties for the new client
         self.__first_name = first_name
         self.__last_name = last_name
         self.__title = title
@@ -100,23 +109,21 @@ class Add():
         self.__overdraft_limit = overdraft_limit
         self.__client = pd.read_csv("data/client_data.csv")
 
-
-
     def add_account(self):
+        """Method to add users to the bank"""
 
-        self.__client.loc[len(self.__client)] = [self.__first_name, self.__last_name, self.__title, self.__account_number, self.__date_of_birth,
+        # Uses the properties listed above to add the client to the CSV files.
+        self.__client.loc[len(self.__client)] = [self.__first_name, self.__last_name, self.__title,
+                                                 self.__account_number, self.__date_of_birth,
                                                  self.__occupation,
                                                  self.__account_balance,
                                                  self.__overdraft_limit]
 
-
-        accountHor = self.__client.loc[self.__client['Account Number'] == self.__account_number]
         print("*" * 108)
         print("You just added the account:")
-        print(accountHor)
+        # Prints out the new account holder
+        print(
+            f"{self.__first_name}   {self.__last_name}   {self.__title}   {self.__account_number}   {self.__date_of_birth}    {self.__occupation}     {self.__account_balance}    {self.__overdraft_limit}")
         print("*" * 108)
 
-
         self.__client.to_csv("data/client_data.csv", index=False)
-
-
