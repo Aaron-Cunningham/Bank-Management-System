923896182249a9ade1a53ed2c1295b20c6b9de4a
Aaron-Cunningham <aaroncunningham222@gmail.com>
2022-11-08 16:59:09 +0000

    Added more objects

--- banking_system/main.py
+++ banking_system/main.py
@@ -1,8 +1,8 @@
 import sys
 from menu import Menu
 import pandas as pd
-import edit as people
-from people import Users
+import edit as clients
+import people as people
 
 
 # Sets the column and row display options
@@ -12,85 +12,84 @@ pd.set_option('display.width', 1000)
 
 
 menu = Menu()
-s = Users(Users)
 
-# Created person object
-client = people.Edit(account=4999)
+# Created client to be edited object
+client_to_be_edited = clients.Edit(account=1898)
 
 # Created a client to add object
-clinet_to_add1 = people.Add("Jeff", "Bezos", "Mr", "Male", 5500, "01/12/1964", "CEO of Amazon", 6857489, -40000)
-clinet_to_add2 = people.Add("Elon", "Musk", "Mr", "Male", 5501, "01/28/1971", "Scientist", 44456743, -45000)
+Jeff_Bazos = clients.Add("Jeff", "Bezos", "Mr", "Male", 5500, "01/12/1964", "CEO of Amazon", 6857489, -40000)
+Elon_Musk = clients.Add("Elon", "Musk", "Mr", "Male", 5501, "01/28/1971", "Scientist", 44456743, -45000)
 
+# Known user and unknown object created
+# 0 - First name
+# 1 - Last name
+# 2 - Date of birth
+known_user = people.Users("Aaron", "Cunningham", "01/29/1999")
+unknown_user = people.Users("Freddie", "Mercury", "09/05/1946")
+user_to_delete = people.Users("Emmanuel", "Plews", "03/25/1991")
+
+# Object created for all clients
+All = people.All_Clients()
+
+
+
+
+def search():
+    # Returns account that exits
+    known_user.account_search()
+    # Returns no account found
+    unknown_user.account_search()
+
+def withdraw():
+    known_user.withdraw(withdraw=500)
+
+def deposit():
+    known_user.deposit(deposit=500)
 
 
 def view_all():
     """Allows you to see the full list of clients"""
     # Views all users
-    s.view_all()
+    All.view_all()
+
 
 def remove():
     """This remove the client 5000 from the dataframe"""
     # 0 - Account Number
-    s.remove(5500)
+    user_to_delete.remove_client()
 
 def edit_first_name():
     """Edits first name of client object"""
 
     # Client object name can be edited
-    client.set_first_name("Aaron")
+    client_to_be_edited.set_first_name(new_name="Jerry")
 def edit_last_name():
     """Edits last name of client object"""
     # Client object last name can be edited
-    client.set_last_name("Cunningham")
+    client_to_be_edited.set_last_name(new_name="Smith")
 def edit_occupation():
     """Edits occupation of client object"""
     # Client object occupation can be edited
-    client.set_occupation("Programmer")
+    client_to_be_edited.set_occupation(new_occupation="Computer Scientist")
 def menu():
     """Runs the UI version of the program"""
     Menu().run()
 
 def add_account():
     """Adds client to add object to the database"""
-    clinet_to_add1.add_account()
-
-def search():
-    """This function is used to search for a client in the csv file"""
-    # Search for client based on name, and date of birth. This is not case-sensitive
-    s.search_by_name(first_name="aaron", last_name="cunningham", date_of_birth="01/29/1999")
-
-def withdraw():
-    """This function withdraws month from an account and
-       adds a 5 pound charge when going over overdraft limit
-    """
-    # 0 - Account number
-    # 1 - Amount to withdraw
-    s.withdraw(account=4999, withdraw=500)
+    Jeff_Bazos.add_account()
 
 def view_negative():
-    """This function allows you to see all users with negative balance"""
-    s.negative_balance()
+    """Returns all clients with a negative balance"""
+    All.view_negative()
 
-def deposit():
-    """This function allows you to depost money into a users account"""
-    # 0 - Account
-    # 1 = Amount to deposit
-    s.deposit(account=4999, deposit=500)
 
-def search_by_name():
-    """This function allows you to search for a user by their name"""
-    # 0 - First name
-    # 1 - Last name
-    # 2 - Date of birth
-    s.search_by_name(first_name="aaron", last_name="cunningham", date_of_birth="01/29/1999")
 
-def search_by_account():
-    """This function allows you to search for a client based on their account number"""
-    # 0 - Account
-    s.search(account=4999)
+
 
 if __name__ == '__main__':
 
     globals()[sys.argv[1]]()
+
 
 
