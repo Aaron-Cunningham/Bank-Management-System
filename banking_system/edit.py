de0d5d2269c75191811dc3446671924f562057c8
aaronc991 <93551130+aaronc991@users.noreply.github.com>
2022-11-08 00:16:16 +0000

    Added account checker

--- banking_system/edit.py
+++ banking_system/edit.py
@@ -1,9 +1,7 @@
-import csv
-
 import pandas as pd
 
 
-class Edit():
+class Edit:
     """This class is used to edit clients details"""
 
     def __init__(self, account):
@@ -44,7 +42,6 @@ class Edit():
 
     def set_last_name(self, new_name):
         self.account_check
-
         print("*" * 108)
         print("Old account details account details: \n"
               "\n", self.get_account)
@@ -78,7 +75,7 @@ class Edit():
 
 class Add:
 
-    def __init__(self, first_name, last_name, title, account_number, date_of_birth, occupation, account_balance,
+    def __init__(self, first_name, last_name, title, pronoun, account_number, date_of_birth, occupation, account_balance,
                  overdraft_limit):
 
         # Exceptions with type errors if wrong information is inputted on the object
@@ -88,6 +85,8 @@ class Add:
             raise TypeError("Last name should be a string")
         if not isinstance(title, str):
             raise TypeError("Title should be a string")
+        if not isinstance(pronoun, str):
+            raise TypeError("Pronoun must be a string")
         if not isinstance(account_number, int):
             raise TypeError("Account number should be an integer")
         if not isinstance(date_of_birth, str):
@@ -97,11 +96,12 @@ class Add:
         if not isinstance(account_balance, int):
             raise TypeError("Account balance should be an Integer")
         if not isinstance(overdraft_limit, int):
-            raise TypeError("Overdraft limit should be an Integer")
+            raise TypeError("Overdraft limit should be an Negative Integer")
         # Properties for the new client
         self.__first_name = first_name
         self.__last_name = last_name
         self.__title = title
+        self.__pronoun = pronoun
         self.__account_number = account_number
         self.__date_of_birth = date_of_birth
         self.__occupation = occupation
@@ -111,9 +111,16 @@ class Add:
 
     def add_account(self):
         """Method to add users to the bank"""
+        # Prevents the user from entering wrong values
+        if self.__account_balance < 0:
+            print("No negative integers for account balance")
+            exit()
+        elif self.__overdraft_limit > 0:
+            print("No positive integers for overdraft limits")
+            exit()
 
         # Uses the properties listed above to add the client to the CSV files.
-        self.__client.loc[len(self.__client)] = [self.__first_name, self.__last_name, self.__title,
+        self.__client.loc[len(self.__client)] = [self.__first_name, self.__last_name, self.__title, self.__pronoun,
                                                  self.__account_number, self.__date_of_birth,
                                                  self.__occupation,
                                                  self.__account_balance,
@@ -123,7 +130,7 @@ class Add:
         print("You just added the account:")
         # Prints out the new account holder
         print(
-            f"{self.__first_name}   {self.__last_name}   {self.__title}   {self.__account_number}   {self.__date_of_birth}    {self.__occupation}     {self.__account_balance}    {self.__overdraft_limit}")
+            f"{self.__first_name}   {self.__last_name}   {self.__title}    {self.__pronoun}    {self.__account_number}   {self.__date_of_birth}    {self.__occupation}     {self.__account_balance}    {self.__overdraft_limit}")
         print("*" * 108)
 
         self.__client.to_csv("data/client_data.csv", index=False)
