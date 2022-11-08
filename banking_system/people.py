256d8e46b14726eeb2c6e5fac285adb2f8e8ffd1
Aaron-Cunningham <aaroncunningham222@gmail.com>
2022-11-08 16:56:25 +0000

    Edited with more of a OOP approach

--- banking_system/people.py
+++ banking_system/people.py
@@ -2,15 +2,100 @@ import pandas as pd
 
 
 # Class definition
-class Users():
+class Users:
 
-    # constructor/initialiser
-    def __init__(self, users):
-        # Users properties
-        self.__users = users
-        # Fee that is changeable
+
+    def __init__(self, first_name, last_name, date_of_birth):
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__date_of_birth = date_of_birth
+        self.__client = pd.read_csv("data/client_data.csv")
         self.__fee = 5
-        # List of clients in the bank
+
+
+    @property
+    def account_check(self):
+        """This checks that an account exists before proceeding with methods"""
+        try:
+            self.get_account_details
+            if self.get_account_details.empty:
+                raise Exception
+        except:
+            print("*" * 35)
+            print("No account found, please try again")
+            print("*" * 35)
+            exit()
+
+
+    @property
+    def get_account_details(self):
+        """Gets the account of the clients account number"""
+
+        return self.__client.loc[self.__client['First name'].str.contains(self.__first_name, case=False) & (
+            self.__client['Last name'].str.contains(self.__last_name, case=False)) & (
+                              self.__client['Date of birth'].str.contains(self.__date_of_birth, case=False))]
+
+    def account_search(self):
+        self.account_check
+        print("*" * 117)
+        print(self.get_account_details)
+        print("*" * 117)
+
+    def withdraw(self, withdraw):
+        self.account_check
+        print("Old account balance: ")
+        print(self.get_account_details)
+
+        # Locates the row of the account that matches input from user and edits balance from users input
+        self.__client.loc[(self.__client['First name'] == self.__first_name) & (self.__client['Last name'] == self.__last_name) &
+                          (self.__client ['Date of birth'] == self.__date_of_birth), ['Account balance']] = self.__client[
+                                                                                                 'Account balance'] - withdraw
+        # Adds £5 fee if the client goes over their overdraft limit
+        self.__client.loc[
+            self.__client['overdraft limit'] > self.__client['Account balance'], 'Account balance'] -= self.__fee
+
+        print(self.get_account_details)
+
+        self.__client.to_csv("data/client_data.csv", index=False)
+
+    def deposit(self, deposit):
+        self.account_check
+        print("Old account balance: ")
+        print(self.get_account_details)
+        # Locates the row of the account that matches input from user and edits balance from users input
+        self.__client.loc[
+            (self.__client['First name'] == self.__first_name) & (self.__client['Last name'] == self.__last_name) &
+            (self.__client['Date of birth'] == self.__date_of_birth), ['Account balance']] = self.__client[
+                                                                                                 'Account balance'] + deposit
+        print(self.get_account_details)
+
+        self.__client.to_csv("data/client_data.csv", index=False)
+
+    def remove_client(self):
+        """Removes client from CSV file"""
+        self.account_check
+        print("*"*117)
+        print(f"{self.get_account_details}")
+        self.__client.drop(self.get_account_details.index, inplace=True)
+        # Updates CSV file
+        print()
+        print("Has now been removed")
+        print("*"*117)
+        # Updates CSV file
+        self.__client.to_csv("data/client_data.csv", index=False)
+
+
+    def negative_balance(self):
+        account = (self.__client.loc[self.__client['Account balance'] < 0])
+        if account.empty:
+            print("No negative accounts")
+            exit()
+
+        print(f"{account}")
+
+
+class All_Clients:
+    def __init__(self):
         self.__client = pd.read_csv("data/client_data.csv")
 
     def view_all(self):
@@ -21,148 +106,14 @@ class Users():
         # Prints all the clients from the bank
         print(f"{self.__client}")
 
-    def search(self, account):
-        if not isinstance(account, int):
-            raise TypeError("Integer number only")
-        """
-        This method searches for an account using the account number, then it prints the account.
-        """
-        account = Users(self.__client.loc[self.__client['Account Number'] == account])
+    def view_negative(self):
 
-        print(f"{account.__users}")
+        self.__client = (self.__client.loc[self.__client['Account balance'] < 0])
 
-    def search_by_name(self, first_name, last_name, date_of_birth):
-        """
-        This method search the clients of the bank by name and date of birth.
-
-        It is not case-sensitive.
-
-        """
-        # While loop while condition is true
-        while True:
-            # Trys to find the account
-            try:
-                __accountHolder = Users(
-                    self.__client.loc[self.__client['First name'].str.contains(first_name, case=False) & (
-                        self.__client['Last name'].str.contains(last_name, case=False)) & (
-                                          self.__client['Date of birth'].str.contains(date_of_birth))])
-                if __accountHolder.__users.empty:
-                    raise Exception
-
-                # Catches exception
-            except:
-                print("No account found, please try again")
-                # Breaks out of while loop when this condition is met
-                break
-            else:
-                # If account is found it will print it
-                print(f"{__accountHolder.__users}")
-                # Breaks out of while loop when condition is met
-                break
-
-    def withdraw(self, account, withdraw):
-        # While loop while condition is true
-        while True:
-            # Trys to find the account
-            try:
-                __account = Users(self.__client.loc[self.__client['Account Number'] == account])
-                if __account.__users.empty:
-                    raise Exception
-
-                # Catches exception
-            except:
-                print("No account found, please try again")
-                # Breaks out of while loop when this condition is met
-                exit()
-            else:
-                # If account is found it will print it
-                print("Old account balance")
-                print(f"{__account.__users}")
-                # Breaks out of while loop when condition is met
-                break
-
-        # Locates the row of the account number that matches input from user and edits balance from users input
-        self.__client.loc[self.__client['Account Number'] == account, ['Account balance']] = self.__client[
-                                                                                                 'Account balance'] - withdraw
-        # Adds £5 fee if the client goes over their overdraft limit
-
-        self.__client.loc[
-            self.__client['overdraft limit'] > self.__client['Account balance'], 'Account balance'] -= self.__fee
-
-        self.__client.to_csv("data/client_data.csv", index=False)
-
-        updatedAccount = Users(self.__client.loc[self.__client['Account Number'] == account])
-
-        print()
-        print("Updated account balance: ")
-        print(f"{updatedAccount.__users}")
-
-    def deposit(self, account, deposit):
-        while True:
-            # Trys to find the account
-            try:
-                __account = Users(self.__client.loc[self.__client['Account Number'] == account])
-                if __account.__users.empty:
-                    raise Exception
-
-                # Catches exception
-            except:
-                print("No account found, please try again")
-                # Breaks out of while loop when this condition is met
-                exit()
-            else:
-                # If account is found it will print it
-                print("Old account balance")
-                print(f"{__account.__users}")
-                print()
-                # Breaks out of while loop when condition is met
-                break
-
-        # Locates the row of the account number that matches input from user and edits balance from users input
-        self.__client.loc[self.__client['Account Number'] == account, ['Account balance']] = self.__client[
-                                                                                                 'Account balance'] + deposit
-
-        updatedAccount = Users(self.__client.loc[self.__client['Account Number'] == account])
-        self.__client.to_csv("data/client_data.csv", index=False)
-        print()
-
-        print("Updated account balance")
-
-        print(updatedAccount.__users)
-
-    def remove(self, account):
-
-        # While loop while condition is true
-        while True:
-            # Trys to find the account
-            try:
-                __account = Users(self.__client.loc[self.__client['Account Number'] == account])
-                if __account.__users.empty:
-                    raise Exception
-
-                # Catches exception
-            except:
-                print("No account found, please try again")
-                # Breaks out of while loop when this condition is met
-                exit()
-            else:
-                # If account is found it will print it
-                print("Account: ")
-                print(f"{__account.__users}")
-                # Breaks out of while loop when condition is met
-                break
-        # Removes client from csv file
-        self.__client.drop(self.__client[self.__client['Account Number'] == account].index, inplace=True)
-        # writes to csv
-        self.__client.to_csv("data/client_data.csv", index=False)
-        print("Client Deleted")
-
-    def negative_balance(self):
-
-        account = (self.__client.loc[self.__client['Account balance'] < 0])
-
-        if account.empty:
+        if self.__client.empty:
             print("No negative accounts")
             exit()
 
-        print(f"{account}")
+
+        print(self.__client)
+
