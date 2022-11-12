93fca188c9b85237dc10862009965e0a3e95f757
Aaron-Cunningham <aaroncunningham222@gmail.com>
2022-11-12 15:21:12 +0000

    Added search unknown function

--- banking_system/main.py
+++ banking_system/main.py
@@ -4,18 +4,15 @@ import pandas as pd
 import edit as clients
 import people as people
 
-
 # Sets the column and row display options
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
 
-
 menu = Menu()
 
 """Created client to be edited object"""
 client_to_be_edited = clients.Edit(account=1898)
-
 
 """Created a client to add object"""
 # 0 - First name
@@ -46,20 +43,21 @@ user_to_delete = people.Users("Stephanus", "Illsley", "06/19/2002")
 # Object created for all clients
 All = people.AllClients()
 
+
 def search():
     """Searches for object matching name and date of birth"""
     # Returns account that exits
     known_user.account_search()
-    # Returns no account found
-    unknown_user.account_search()
+
 
 def withdraw():
     """Withdraws money from known user object account"""
-    known_user.withdraw(withdraw=500)
+    known_user.withdraw(withdraw=100)
+
 
 def deposit():
     """Deposits money into known user object account"""
-    known_user.deposit(deposit=500)
+    known_user.deposit(deposit=100)
 
 
 def view_all():
@@ -73,35 +71,45 @@ def remove():
     # 0 - Account Number
     user_to_delete.remove_client()
 
+
 def edit_first_name():
     """Edits first name of client object"""
 
     # Client object name can be edited
     client_to_be_edited.set_first_name(new_first_name="Jason")
+
+
 def edit_last_name():
     """Edits last name of client object"""
     # Client object last name can be edited
     client_to_be_edited.set_last_name(new_last_name="Berkley")
+
+
 def edit_occupation():
     """Edits occupation of client object"""
     # Client object occupation can be edited
     client_to_be_edited.set_occupation(new_occupation="Data Engineer")
+
+
 def menu():
     """Runs the UI version of the program"""
     Menu().run()
 
+
 def add_account():
     """Adds client to add object to the database"""
     Jeff_Bazos.add_account()
+
 
 def view_negative():
     """Returns all clients with a negative balance"""
     All.view_negative()
 
 
+def search_unknown():
+    # Returns no account found
+    unknown_user.account_search()
+
+
 if __name__ == '__main__':
-
     globals()[sys.argv[1]]()
-
-
-
